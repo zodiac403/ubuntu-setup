11ac94b687c4ebab75a1a110603ea20208608943
zodiac403 <zodiac403@gmx.de>
2023-10-02 14:31:44 +0200

    pull.rebase true

--- src/git-config.sh
+++ src/git-config.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 echo "== CONFIGURE GIT =="
+git --version
 
-git config --global core.editor vim &&
-    git config --global help.autocorrect 1 &&
-    git config --global push.autosetupremote true
+git config --global core.editor vim
+git config --global help.autocorrect 1
+git config --global pull.rebase true
+git config --global push.autosetupremote true
+
 git config --global --list
 
 echo "== SUCCESS =="
