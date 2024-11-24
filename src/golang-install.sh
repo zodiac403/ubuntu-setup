8da94d3fc57171c9e8e296192f7f4a2752c6b397
zodiac403 <zodiac403@gmx.de>
2024-11-24 15:12:27 +0100

    Install tooling

--- src/golang-install.sh
+++ src/golang-install.sh
@@ -5,6 +5,14 @@ echo "== INSTALL GOLANG =="
 echo "=== SNAP INSTALL ==="
 sudo snap install go --classic
 
+echo "=== GO INSTALL TOOLING ==="
+go install -v \
+  github.com/go-delve/delve/cmd/dlv@latest
+
+echo "=== CODIUM INSTALL EXTENSIONS ==="
+codium --install-extension \
+  golang.Go
+  
 echo "=== VERIFY VERSIONS ==="
 echo "go version" && go version
 
